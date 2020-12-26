86d08405514fa122b4338bef3a025e99e526e65c
Adeyemi Jammal <32106343+Jammal7@users.noreply.github.com>
2020-12-26 12:01:10 -0800

    Final Push

---
+++ script.js
@@ -0,0 +1,23 @@
+const signin_btn = document.querySelector("#sign-in-btn");
+const signup_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
+
+signup_btn.addEventListener("click", () => {
+  container.classList.add("sign-up-mode");
+});
+
+signin_btn.addEventListener("click", () => {
+  container.classList.remove("sign-up-mode");
+});
+
+
+
+
+
+
+
+
+
+
+
+
